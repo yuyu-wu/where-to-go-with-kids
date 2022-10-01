f5b9634edaa03d40490eda4ba214842808829d2d
yuyu <swharriet2012@gmail.com>
2022-10-01 22:27:35 +1000

    add idea model

---
+++ models/ideas.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ideaSchema = new Schema({
+    title: String,
+    description: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Idea = mongoose.model('Idea', ideaSchema);
+module.exports = Idea;
