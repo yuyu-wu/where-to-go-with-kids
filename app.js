3f4db76fe0f67926e3d174d097c752c251c372e6
yuyu <swharriet2012@gmail.com>
2022-10-24 22:46:21 +1100

    update error in app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const engine = require('ejs-mate');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Idea = require('./models/idea');
+const catchAsync = require('./error/catchAsync');
+const ExpressError = require('./error/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/weekend')
     .then(() => {
@@ -30,60 +32,75 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
 app.get('/ideas/new', (req, res) => {
     res.render('ideas/new')
-})
+});
 
-app.get('/ideas', async (req, res) => {
+app.get('/ideas', catchAsync(async (req, res) => {
     const ideas = await Idea.find({});
     res.render('ideas/index', {ideas});
-})
+}));
 
-app.post('/ideas', async (req, res) => {
+app.post('/ideas', catchAsync(async (req, res) => {
+    if (!req.body.idea) {
+        throw new ExpressError('Invalid Idea Data', 404);
+    }
     const idea = new Idea(req.body.idea);
     await idea.save();
     res.redirect('/ideas')
-})
+}));
 
-app.get('/ideas/:id', async(req, res) => {
+app.get('/ideas/:id', catchAsync(async(req, res) => {
     const idea = await Idea.findById(req.params.id)
     if (!idea) {
         return res.redirect('/ideas');
     }
     res.render('ideas/show', {idea});
-})
+}));
 
-app.get('/ideas/:id/edit', async (req, res) => {
+app.get('/ideas/:id/edit', catchAsync(async (req, res) => {
     const idea = await Idea.findById(req.params.id);
     res.render('ideas/edit', {idea});
-})
+}));
 
-app.put('/ideas/:id', async (req, res) => {
+app.put('/ideas/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const idea = await Idea.findByIdAndUpdate(id, {...req.body.idea});
     res.redirect(`/ideas/${idea._id}`);
     // res.send(req.params)
-})
+}));
 
-app.delete('/ideas/:id', async (req, res) => {
+app.delete('/ideas/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await Idea.findByIdAndDelete(id);
     res.redirect('/ideas');
-})
+}));
 
 app.get('/register', (req, res) => {
     res.render('users/register')
-})
+});
 
 app.get('/login', (req, res) => {
     res.render('users/login')
-})
+});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+});
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if (!err.message) {
+        err.message = 'Something went wrong!';
+    }
+    res.status(statusCode).render('error', {err});
+});
 
 app.listen(3000, () => {
     console.log('Listening on 3000!')
-})
+});
 
 
 
