ebd9a35ca7cc5a47ffcf42998c8d64bdb829d305
yuyu <swharriet2012@gmail.com>
2022-11-11 15:46:47 +1100

    add start script

--- app.js
+++ app.js
@@ -213,8 +213,9 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send('oh no')
 });
 
-app.listen(3000, () => {
-    console.log('Listening on 3000!')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
 
 
